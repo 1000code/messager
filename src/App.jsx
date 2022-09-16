0c8d2aaa86a435232684a61de9c650d22eb7bcc7
Sonephet Minalavong <sonephet.sm@laogw.la>
2022-09-16 11:55:58 +0700

    11

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 import MessengerCustomerChat from "react-messenger-customer-chat";
+import YouTube from "react-youtube";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const onPlayerReady = (event) => {
+    event.target.pauseVideo();
+  };
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
   return (
     <div className="App">
+      <div className="">
+        <YouTube videoId="jV7MYUhMavI" opts={opts} onReady={onPlayerReady} />
+      </div>
       <h1 className="s">Test</h1>
       <MessengerCustomerChat pageId="107649348764001" appId="839133677251380" />
 
